18cd191b7b68eac1c783cd70eb3141100cc3fb53
Daniel Robaszkiewicz <daniel.robaszkiewicz@wp.pl>
2024-04-26 18:01:42 +0200

    Added reading features from Camera

---
+++ src/EventPi.Services.Camera/GrpcFrameFeaturesService.cs
@@ -0,0 +1,22 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using EventPi.Services.Camera;
+
+namespace EventPi.Services.Camera;
+
+public class GrpcFrameFeaturesService : FrameFeatures.FrameFeaturesBase
+{
+    private readonly ILogger<GrpcFrameFeaturesService> _logger;
+
+    public GrpcFrameFeaturesService(ILogger<GrpcFrameFeaturesService> logger)
+    {
+        _logger = logger;
+    }
+    public override async Task<Empty> Process(FrameFeaturesRequest request, ServerCallContext context)
+    {
+        return new Empty();
+    }
+
+
+}
