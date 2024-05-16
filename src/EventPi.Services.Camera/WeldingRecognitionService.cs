69ece6aaea5a26df1ec9e0ff0b551b56eac24257
Daniel Robaszkiewicz <daniel.robaszkiewicz@wp.pl>
2024-05-16 22:06:55 +0200

    added pid control

--- src/EventPi.Services.Camera/WeldingRecognitionService.cs
+++ src/EventPi.Services.Camera/WeldingRecognitionService.cs
@@ -1,4 +1,5 @@
-﻿using EventPi.Services.Camera.Contract;
+﻿using EventPi.Pid;
+using EventPi.Services.Camera.Contract;
 using Microsoft.Extensions.Logging;
 
 namespace EventPi.Services.Camera;
@@ -15,7 +16,13 @@ public class WeldingRecognitionService
     public ICameraParametersReadOnly WeldingProfile { get; set; }
     public double DetectWeldingBound { get; set; }
     public double WeldingBrightPixelsTarget { get; set; }
+    public double CurrentAppliedShutter { get; set; }
     public double DetectNonWeldingBound { get; set; }
+    public double KP { get; set; }
+    public double KD { get; set; }
+    public double KI { get; set; }
+    public double OutputLowerLimit { get; set; }
+    public double OutputUpperLimit { get; set; }
     public bool TryDetect { get; set; }
     public WeldingRecognitionService(ILogger<WeldingRecognitionService> logger,GrpcCppCameraProxy proxy, GrpcFrameFeaturesService gprc)
     {
@@ -27,6 +34,9 @@ public class WeldingRecognitionService
         DefaultProfile = new CameraProfile();
         DetectWeldingBound = 400 * 400 * 0.8;
         DetectNonWeldingBound = 400 * 400 * 0.8;
+        KP = 0.01;
+        OutputLowerLimit = -100;
+        OutputUpperLimit = 100;
     }
 
     private void OnDetectWelding(object? sender, FrameFeaturesRecord e)
@@ -37,8 +47,10 @@ public class WeldingRecognitionService
         {
             IsWelding = true;
             _logger.LogInformation("Welding detected");
-            _proxy.ProcessAsync(_cameraParameters.CopyFrom(WeldingProfile));
-          
+            _cameraParameters = new SetCameraParameters();
+            _cameraParameters.CopyFrom(WeldingProfile);
+            _proxy.ProcessAsync(_cameraParameters);
+            CurrentAppliedShutter = _cameraParameters.Shutter;
         }
         else
         {
@@ -46,17 +58,21 @@ public class WeldingRecognitionService
             {
                 _logger.LogInformation("Welding not detected");
                 IsWelding = false;
-                _proxy.ProcessAsync(_cameraParameters.CopyFrom(DefaultProfile));
+                _cameraParameters = new SetCameraParameters();
+                _cameraParameters.CopyFrom(DefaultProfile);
+                _proxy.ProcessAsync(_cameraParameters);
+                CurrentAppliedShutter = _cameraParameters.Shutter;
             }
         }
 
         if (IsWelding)
         {
-            if (e.TotalBrightPixels > WeldingBrightPixelsTarget)
-            {
-                _cameraParameters.Shutter -= 50;
-                _proxy.ProcessAsync(_cameraParameters);
-            }
+            PidController pid = new PidController(KP, KD, KI, OutputUpperLimit, OutputLowerLimit);
+            var result =pid.CalculateOutput(WeldingBrightPixelsTarget,e.TotalBrightPixels, TimeSpan.FromSeconds(1));
+
+            _cameraParameters.Shutter += (int)result;
+            _proxy.ProcessAsync(_cameraParameters);
+            CurrentAppliedShutter = _cameraParameters.Shutter;
         }
       
     }
