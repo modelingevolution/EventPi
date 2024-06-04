1ca2f03936841c1e3169a0badae1463f5548fd5d
Rafał Maciąg <rafal.maciag@modelingevolution.com>
2024-06-04 21:30:32 +0200

    Dockerfile

---
+++ src/EventPi.AutoUpdate/Class1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace EventPi.AutoUpdate
+{
+    public class UpdateProcessManager(IConfiguration configuration)
+    {
+
+        public async Task<bool> IsNewVersionAvailable()
+        {
+            return false;
+        }
+        public async Task Update()
+        {
+            
+        }
+    }
+}
