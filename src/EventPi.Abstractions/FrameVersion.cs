3b07f8290640989d9321bc8d41301bd3396fba4d
Daniel Robaszkiewicz <daniel.robaszkiewicz@wp.pl>
2025-01-21 17:07:23 +0100

    changes

---
+++ src/EventPi.Abstractions/FrameVersion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPi.Abstractions
+{
+    public class FrameVersion
+    {
+        public string Version { get; set; }
+    }
+}
